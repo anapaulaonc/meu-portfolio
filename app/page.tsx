0b103e8a6a596a4f2c34ea47457ea31f322f21a4
anapaulaonc <anapaulaoncosta@gmail.com>
2025-06-09 01:12:41 -0300

    corrections

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,7 @@
 
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Textarea } from "@/components/ui/textarea"
+
 import {
   ArrowRight,
   Download,
